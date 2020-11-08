950bc0ec798e30ff91971ab5af75f320e2d2b65d
CELTIANqzq <9697570@qq.com>
2020-11-08 16:13:31 +0800

    更新测试对评分的错误的选取

--- Datacleaning.py
+++ Datacleaning.py
@@ -1,36 +1,26 @@
-import sys
-from datetime import datetime
+import  pandas  as pd
 import re
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+df=pd.read_csv('Train_test.csv')#这个会直接默认读取到这个Excel的第一个表单
+pd.set_option('display.max_columns',None) #设置显示列数
+pd.set_option('display.max_rows',None) #设置显示行数
 def if_contain_symbol(keyword):
     if re.search(r"\W", keyword):
         return True
     else:
         return False
+#判断评分在不在0-5之间
+# for row in df.itertuples():
+#   if(row.average_rating>0 and row.average_rating<5):
+#      print(row.bookID)
 
-def map():
-    count=0
-    for line in sys.stdin:
-        count=count+1
-        if(count>1):
-          data = line.strip().split()
-          print(data)
-          bookID = float(data[0])
-          title = str(data[1])
-          author = str(data[2])
-          average_rating = float(data[3])
-          isbn = float(data[4])
-          isbn13=float(data[5])
-          language_code=str(data[6])
-          num_page=int(data[7])
-          ratings_count=int(data[8])
-          text_reviews_count=int(data[9])
-          publication_date=datetime.strptime(data[10], '%Y-%m-%d')
-          publisher=str(data[11])
-          print(bookID)
-          # if(average_rating>0 and average_rating<5 and if_contain_symbol(title)==False and if_contain_symbol(author)==False):
-          #     print(bookID)
-        #print("\t".join([key, value]))
-
-if __name__ == "__main__":
-    map()
+# 检测乱码 //to do
+for row in df.itertuples():
+  # if(if_contain_symbol(row.title)==False):
+  res=row.title
+  res = res.encode('utf-8').decode('latin-1')
+  print(res)
+# df.to_csv("out.csv", index_label = "index")
+# for line in df.bookID:
+#    print(line)
